247638d02b5b0de4399df63e46a8b1aea7094c9d
Frederick <fjohnson@spartaglobal.com>
2022-02-23 11:53:27 +0000

    stuff

---
+++ terraform/main.tf
@@ -0,0 +1,11 @@
+# Terraform init -will download any required packages
+
+# provider aws
+provider "aws" {
+  
+
+# which region
+   region = "eu-west-1"
+}
+# init with terraform `terraform init`
+# what do we want to lauch
